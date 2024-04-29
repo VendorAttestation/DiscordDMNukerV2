ac18d69c41f596f40220d0a1d02e74f96875a9f0
TheVisual <132447890+TheVisual@users.noreply.github.com>
2024-04-29 12:50:45 -0500

    Release
    Finish Project

--- README.md
+++ README.md
@@ -10,30 +10,50 @@ DiscordDMNukerV2 is powered by discord.js-selfbot-v13 This means for some reason
 - Audit log dumper
 
 # Current Features
+- Deletes ALL Dms from a specific user/group
+- Edits ALL Dms from a specific user/group
+- Extended Logging
 - Async
 - Token Support
 - Token Session Saving when reopening application.
+- Save Images / Videos (Multi Attachments supported!)
+- Save Logs Of messages
+- Group Chat Support
+- Guild Channel Support
 - Linux Support
 - Mac Support
 - Windows Support
-- Node JS 16+
+- Simple Anti-Bot (Realistic)Delays
+- Node.js LTS 16.6.0 or newer is required
 
 # Development roadmap [❌] = In progress [✔️] = Done [⚠️] = No Longer Maintained (Working but may break anytime)
-- Extended Logging [❌]
+- Extended Logging [✔️]
 - Token Support  [✔️]
 - Token Session Saving when reopening application. [✔️]
 - Async [✔️]
 - Latest Discord API [✔️]
-- Save Images / Videos [❌]
-- Save Logs Of messages [❌]
-- Message Editor Support [❌]
-- Group Chat Support [❌]
-- Channel Message Deleter In guilds [❌]
-- Audit log dumper [❌]
+- Save Images / Videos [✔️]
+- Save Logs Of messages [✔️]
+- Message Editor Support [✔️]
+- Group Chat Support [✔️]
+- Channel Message Deleter In guilds [✔️]
 
 # Credits
 - https://github.com/TheVisual (Program)
 - https://github.com/aiko-chan-ai/discord.js-selfbot-v13 (Library)
 
 # Dependencies
-- https://github.com/aiko-chan-ai/discord.js-selfbot-v13 (Master Branch Used)
+- https://github.com/aiko-chan-ai/discord.js-selfbot-v13 (Master Branch Used Via NPM)
+
+# Donate
+- BTC: bc1qt3z9gfeev4ckkl3mwns0mwe33zjl99whk690ra
+- LTC: LcDbfomhoru4Pd2Y8QidXhn8Ry2B2oxSCg
+- XMR: 45RERZLWrUnip3Wa9Tk9np2Xm1CF79fLpTbErBHCF2XnVbXmpixhAJp1FFNSHkQ2zHcvWVPpxXxat4e6YjFCK4D5MCpRxUG
+- BCH: qzzvlrd95ms2zurr43nzcxxnxy2lr3gn3ux0mwnskt
+- ETH: 0x15f73A7a9f01a6ee3A3d38A8399817bB712196d4
+- TRX (Tron): TXbXSFBF5H94z3YedGkuaHP7ZkkGFDzcBD
+
+# How To use
+- Edit .env with info
+- Type .delete in any channel/dm/groupchat to delete messages
+- Type .edit in any channel/dm/groupchat to edit messages
--- dmnuker.js
+++ dmnuker.js
@@ -1,41 +1,141 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
 const { Client } = require('discord.js-selfbot-v13');
+const { v4: uuidv4 } = require('uuid');
 const client = new Client();
-const config = require('./config.json');
-const environmentConfig = config['settings'];
 
-function Delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+const editedText = process.env.EDITED_TEXT;
+const minDelay = parseInt(process.env.MIN_DELAY, 10) || 500;
+const maxDelay = parseInt(process.env.MAX_DELAY, 10) || 2000;
+
+const allowedExtensions = [
+    ".mp4", ".avi", ".mov", ".mkv", ".flv", ".wmv", ".webm", // Video
+    ".jpg", ".jpeg", ".png", ".gif", // Image
+    ".mp3", ".wav", ".ogg", ".flac", ".m4a", // Audio
+    ".txt", ".json", // Text
+];
+
+if (!editedText) {
+    console.error('Edited text is empty. Please check your .env file.');
+    process.exit(1);
 }
 
-client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
-
-  if (!environmentConfig.user_id_of_person) {
-    console.error("NEED USER ID!");
-    return;
-  }
-
-  if (!environmentConfig.discord_token) {
-    console.error("NEED DISCORD TOKEN");
-    return;
-  }
-
-  try {
-    const user = await client.users.fetch(environmentConfig.user_id_of_person);
-    if (!user) return;
-
-    const channel = await user.createDM();
-    if (!channel) return;
-
-    await channel.messages.fetch().forEach(async message => {
-      if (message.author.id != environmentConfig.user_id_of_person) {
-        await Delay(1000);
-        await message.delete();
-      }
+(async () => {
+    client.on('ready', async () => {
+        console.log(`${client.user.username} is ready!`);
     });
-  } catch (error) {
-    console.error(error);
-  }
-});
 
-client.login(environmentConfig.discord_token);
+    client.on('messageCreate', async message => {
+        if (message.author.id === client.user.id) {
+            if (message.content.toLowerCase() === '.delete') {
+                const messages = await fetchAndLogMessages(message, 'delete_history.txt');
+                await deleteMessages(messages);
+            } else if (message.content.toLowerCase() === '.edit') {
+                const messages = await fetchAndLogMessages(message, 'edit_history.txt');
+                await editMessages(messages, editedText);
+            }
+        }
+    });
+
+    async function fetchAndLogMessages(message, fileName) {
+        const channel = message.channel;
+        let folderName = channel.type === 'GUILD_TEXT' ? 'guild-' + channel.guild.name + '-' + channel.name : 'dm-' + channel.recipient?.username;
+        if (channel.type === 'GROUP_DM') {
+            folderName = 'group-' + channel.name;
+        }
+
+        const baseDir = path.join(__dirname, 'channel_history');
+        const channelDir = path.join(baseDir, folderName.replace(/[^a-zA-Z0-9-]/g, '_'));
+        const historyPath = path.join(channelDir, fileName);
+        const attachmentDir = path.join(channelDir, 'attachments');
+
+        fs.mkdirSync(channelDir, {
+            recursive: true
+        });
+        fs.mkdirSync(attachmentDir, {
+            recursive: true
+        });
+
+        let allMessages = [];
+        let lastId;
+        while (true) {
+            const options = {
+                limit: 100,
+                before: lastId
+            };
+            const messages = await channel.messages.fetch(options);
+            if (messages.size === 0) break;
+
+            messages.forEach(msg => {
+                allMessages.push(msg);
+                fs.appendFileSync(historyPath, `${msg.author.tag}: ${msg.content}\n`);
+                msg.attachments.forEach(async (attachment) => {
+                    const url = attachment.url.split('?')[0];
+                    const extension = path.extname(url).toLowerCase();
+                    if (allowedExtensions.includes(extension)) {
+                        const attachmentPath = path.join(attachmentDir, `${uuidv4()}${extension}`);
+                        const response = await axios({
+                            method: 'get',
+                            url: attachment.url,
+                            responseType: 'stream'
+                        });
+                        response.data.pipe(fs.createWriteStream(attachmentPath));
+                    }
+                });
+            });
+            lastId = messages.last().id;
+        }
+        return allMessages;
+    }
+
+    const skippableMessageTypes = [
+        "GUILD_MEMBER_JOIN",
+        "CHANNEL_PINNED_MESSAGE",
+        "USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1",
+        "USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2",
+        "USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3",
+        "USER_PREMIUM_GUILD_SUBSCRIPTION",
+        "THREAD_CREATED",
+        "THREAD_STARTER_MESSAGE",
+        "RECIPIENT_ADD",
+        "RECIPIENT_REMOVE",
+        "CALL",
+        "CHANNEL_NAME_CHANGE",
+        "CHANNEL_ICON_CHANGE"
+    ];
+
+    async function deleteMessages(messages) {
+        for (const msg of messages) {
+            if (msg.author.id !== client.user.id || skippableMessageTypes.includes(msg.type)) {
+                continue;
+            }
+            await delayRandom(minDelay, maxDelay);
+            await msg.delete().catch(console.error);
+        }
+        console.log('All messages deleted.');
+    }
+
+    async function editMessages(messages, newText) {
+        if (!newText) {
+            console.error('No text provided for editing messages.');
+            return;
+        }
+        for (const msg of messages) {
+            if (msg.author.id !== client.user.id || skippableMessageTypes.includes(msg.type)) {
+                continue;
+            }
+            await delayRandom(minDelay, maxDelay);
+            await msg.edit(newText).catch(console.error);
+        }
+        console.log('All messages edited.');
+    }
+
+    async function delayRandom(min = 500, max = 2000) {
+        const delayTime = Math.floor(Math.random() * (max - min + 1) + min);
+        return new Promise(resolve => setTimeout(resolve, delayTime));
+    }
+
+    client.login(process.env.DISCORD_TOKEN);
+})();
